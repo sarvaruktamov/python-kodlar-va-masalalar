b3eb66bbb477f087a2ea06a400c7bdd450bcf564
Sarvar Uktamov <119126105+sarvaruktamov@users.noreply.github.com>
2023-01-10 21:33:17 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,363 @@
+# lugat = {
+#     'boolean':'mantiqiy qiymat',
+#     'float':'onlik son',
+#     'for':'amalni qayta bajarish',
+# }
+# print("foydalanuvchi royhatlari:")
+# royhat =['boolean','float','for']
+# for lugatlar in lugat :
+#     if lugatlar in royhat:
+#         print(f"{lugatlar.title()}{lugat[lugatlar]}")
+# davlatlar ={
+# 'rossiya':'moskva',
+# 'italiya':'rim',
+# 'malayziya':'kuala lumpur',
+# 'qozoqiston':'nursulton',
+# 'qirgiziston':'bishkek',
+# 'singapur':'sidney',
+#
+#   }
+#
+#
+# ington','moskva','rim','nursulton','kuala-lumpur','bishkek','aidney']
+#   user=inpoytaxt=['washput('qaysi davlatlarning poytaxtini bilishni hohlaysiz:>>>')
+#   for in davlatlar.items():
+#       print(f"{user}ning poytaxti{b.items}")
+#   else:
+#       print('kechirasiz bizda bu haqida malumot yuq')
+# a = float(input("birinchi sonni kiriting"))
+# b = float(input("ikkinchi sonni kiriting"))
+# if a > b:
+#     print(f"siz kiritgan birinchi son ikkinchi sondan katta {a > b}")
+# else:
+#     print(f"siz kiritgan birinchi son ikkinchi sondan kichik {a>b}")
+# son =[1245, 3432, 3452,9251]
+# a = min(son)
+# b = max(son)
+# c= sum(son)
+# print(f'minuim son{a},maximal son {b}, jami sum{c}')
+
+# a = float(input('birinchi sinf bolalar sonnini kiriting'))
+# b = float(input('ikkinchi sinf bolalar sonnini kiriting'))
+# c =float(input('uchinchi sinf bolalar sonnini kiriting'))
+# print(f'1-sinfga {a/2}ta para,{a}stol  2-sinfga{b/2}ta parta {b}ta stol')
+# davlat= {
+# 'davlat':'uzbekiston'
+# 'hududi':'448978 kv.km'
+# 'aholisi':'33000000 kishi'
+# 'pul birligi':'sum'
+# }
+# print(input('sizga qaysi davlat kerak?'))
+#  davlatlar ={
+# 'rossiya':'moskva',
+# 'italiya':'rim',
+# 'malayziya':'kuala lumpur',
+# 'qozoqiston':'nursulton',
+# 'qirgiziston':'bishkek',
+# 'singapur':'sidney',
+#
+#   }
+
+#
+# print=['washington','moskva','rim','nursulton','kuala-lumpur','bishkek','aidney']
+#   user=inpoytaxt=['washput('qaysi davlatlarning poytaxtini bilishni hohlaysiz:>>>')
+#   for in davlatlar.items():
+#       print(f"{user}ning poytaxti{b.items}")
+#   else:
+#       print('kechirasiz bizda bu haqida malumot yuq')
+
+# davlatlar=['ozbekistoning poytaxti toshkent','hududi 448978 kv.km','aholisi 33000000','pul birligi sum']
+# user=davlatlar[(input('qaysi davlat haqida bilishni hohlaysiz>>>>'))
+#
+#  for davlat in davlatlar.items():
+#     print(f"{user}haqida malumot bor{b.items}")
+# else:
+#     print('kechirasiz bizda bu haqida malumot yuq')
+
+# ozbekiston ={
+#     'hududi':'car0 = {
+#         'model':'lacetti',
+#         'rang':'oq',
+#         'yil':2018,
+#         'narh':13000,
+#         'km':50000,
+#         'korobka':'avtomat'
+#         }
+# # '
+# ism = input("Ismingiz nima? ")
+# print(f'Salom, {ism.title()}')
+# '
+# .items() metodlari
+# talaba_0 = {
+#     'ism':'sarvar',
+#     'familiya':'uktamov',
+#     'yosh':16,
+#     'kollej':'avtomobilsozlik',
+#     'kurs':1,
+#     }
+# print(talaba_0.items())
+#  NESTING
+# car0={
+#      'model':'lacetti',
+#      'rangi':'oq'
+#      'nomeri':s725oa,
+#    'narh':'1300'
+#   'korobka':'mexanika'
+#  }
+#  # car1={
+#  #     'model':'nexia 3'
+#  #     'rangi':'oq'
+#  #     'nomeri':'w960pa'
+#  #    'narxi':'9000'
+#  #    'korobka':'mexanika'
+#  # }
+# car = car0
+# print(f"{car['model'].title()},\
+#   {car['rang']} rang,\
+#   {car['nomer']}-nomer, {car['narh']}$")
+# car0 = {
+#         'model':'lacetti',
+#         'rang':'oq',
+#         'nomeri':725,
+#         'narh':13000,
+#         'km':50000,
+#         'korobka':'mexanika'
+#         }
+#
+# car1 = {
+#         'model':'nexia 3',
+#         'rang':'oq',
+#         'nomeri':960,
+#         'narh':9000,
+#         'km':  4500,
+#         'korobka':'mexanika'
+#         }
+#
+# car2 = {
+#         'model':'damas',
+#         'rang':'oq',
+#         'nomeri':143,
+#         'narh':15000,
+#         'km':20000,
+#         'korobka':'mexanika'
+#         }
+# car = car0
+# print(f"{car['model'].title()},\
+#   {car['rang']} rang,\
+#   {car['nomeri']}-nomer, {car['narh']}")
+#
+# car = car1
+# print(f"{car['model'].title()},\
+#   {car['rang']} rang,\
+#   {car['nomeri']}nomeri, {car['narh']}")
+#
+# car = car2
+# print(f"{car['model'].title()},\
+#   {car['rang']} rang,\
+#   {car['nomeri']}-nomer, {car['narh']}")
+# while sikle
+# ism =input("ismingiz nima")
+# print(f'salom  qandaysiz ahvollar yaxshimi ishlar joyidami?{ism.title()}')
+# ism = input("ismingiz nima")
+# savol = f"salom,{ism.title()}.yoshingiz nechida? boyingiz necha sm"
+# yosh = input(savol)
+# lugat ichidagi royxat
+# programmer = {
+# 'sarvar':['python','html css'],
+# 'daler':['java','c++'],
+# 'diyor':['html','css'],
+# 'olim':['java'],
+# }
+# for ism, tillar in programmer.items():
+#     print(f"\n{ism.title()}quyidagi dasturlash tillarini biladi")
+#     for til in tillar:
+#         print(f"{til.upper()}")
+#
+#   "ozbekiston":{
+#     'poytaxti':'toshkent',
+#     'hududi':'448978',
+#     'aholisi':'33_000_000',
+#     'pul birligi':'som'
+# },
+#     "rossiya":{
+#     'poytaxti':'moskva',
+#     'maydoni':'17_098_246',
+#     'aholisi':'144_000_000',
+#     'pul birligi':'rubl'
+# }
+#    "aqsh":{
+#     'poytaxti':'washington',
+#     'maydoni':'17_098_246',
+#     'aholisi':'144_000_000',
+#     'pul birligi':'dollor'
+# }
+#     "malaysiya":{
+#     'poytaxti':'kuala lampur',
+#     'maydoni':'17_098_246',
+#     'aholisi':'144_000_000',
+#     'pul birligi':'ringit'
+# },13
+
+#
+# }
+# davlat =input("davlat nomini kiriting:").lower()
+# if davlat in davlatlar:
+#     info = davlatlar[davlat]
+#
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting "
+# savol += "(dasturni to'xtatish uchun 'ext' deb yozing): "
+# qiymat = ''
+# while qiymat != 'exit':
+#     qiymat = input(savol)
+#     if qiymat != 'exit':
+#         print(float(qiymat)**2)
+
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# ishora = True
+# while ishora:
+#     qiymat = input(savol)
+#     if qiymat == 'exit':
+#         ishora = False
+#     else:
+#         print(float(qiymat)**2)
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     if son == 5:
+#        continue
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+
+# son = 0
+# while son<10:
+#
+#     if son%2!=:
+#         continue
+#     else:
+#         print(son)
+# son = 1
+# while son>0:
+#     son += 1
+#     if son%2!=0:
+#        continue
+#     else:
+#         print(son)
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "yoqtirgan kitobingizni kiriting "
+# savol += "(dasturni to'xtatish uchun 'ext' deb yozing): "
+#
+#
+# while True:
+#     qiymat = input(savol)
+#     if qiymat == 'exit':
+#         break
+
+
+# savol = "yoshingizni kiriting:"
+# while True:
+#     qiymat = inpu(savol)
+#     if qiymat == "exit"or qiymat =='quit':
+#         savol = "Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+#         savol += "Musbat son kiriting "
+#         savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# #
+#         while True:
+#             qiymat = input(savol)
+#             if qiymat < 0:
+#                 continue
+#             elif qiymat == 'Exit':
+#                 break
+#             else:
+#                 ildiz = float(qiymat) ** (0.5)
+#                 print(f"{qiymat} ning ildizi {ildiz} ga teng")
+# ism = "komlijon ,'sarvar','asila'"
+# print(input("ismingizni kiriting"))
+#
+# print(f"salom{ism}")
+# son = 1
+# while son<=100000:
+#     print(son, end=' ')
+#     son = son+1
+# ism =(input("ismingiz nima?"))
+# print("yaxshi")
+# yosh = (input("yoshingiz nechida"))
+# print("hali yosh ekansiz")
+# funksiyasidan oddiy qiymat qaytarish
+# def toliq_ism_yasa(ism, familiya):
+#     """Toliq isma qaytaruvchi funksiya"""
+#     toliq_ism = f"{ism} {familiya}"
+#     return toliq_ism
+# talaba1 = toliq_ism_yasa('sarvar','uktamov')
+# talaba2 = toliq_ism_yasa('daler','erkinov')
+# print(f"Darsda bilimdon talabalar: {talaba1} va {talaba2}")
+# def bahola(ismlar):
+#     baholar = {}
+#     while ismlar:
+#         ism = ismlar.pop()
+#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+#         baholar[ism]=baho
+#     return baholar
+#
+# talabalar = ['sarvar', 'daler', 'diyor', 'husan']
+# baholar = bahola(talabalar)
+# # print(baholar)
+# talabalar = ['ali', 'vali', 'hasan', 'husan']
+# baholar = bahola(talabalar[:])
+# print(talabalar)
+# ismlar = ['ali', 'vali', 'hasan', 'husan']
+# katta_harf(ismlar)
+# print(ismlar)
+# ismlar = ['ali', 'vali', 'hasan', 'husan']
+# yangi_ismlar = katta_harf(ismlar)
+# print(ismlar)
+# print(yangi_ismlar)
+# def summa(*sonlar):
+#     """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+#     yigindi = 0
+#     for son in sonlar:
+#         yigindi += son
+#     return yigindi
+# def summa(*sonlar):
+#     """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+#     return sum(sonlar)
+#
+# print(summa(4,5,6,7))
+# def summa(x,y,*sonlar):
+#     """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+#     return x+y+sum(sonlar)
+# ozgaruvchilar
+# ism='sarvar'
+# print(ism)
+# ism='anvar'
+# print(ism)
+
+
+#
+# car0={
+#     'model':"lacetti",
+#     'rangi':'oq',
+#     'yili':2018,
+#     'narh':13000,
+#     'km':50000,
+# }
+# car = car0
+# print(f"{car['model'].title()},\
+# {car['rangi']}rang,\
+# {car['yili']}-yili,{car['narh']}
+
+#
+# def salom_ber():
+#     """salom  beruvchi funksiya"""
+#     print("assalomu alaykum")
+#
+# mehmonlar=['sarvar','alisher','daler']
+# if mehmonlar in mehmonlar:
+#     print(f"assalomu alaykum",{mehmonlar},"sizni ertalabki nahorgi oshga taklif etamiz")
+# else:
+#     print(f"ertalabki nahorga",'{mehmonlar}',"sizni taklif etamiz")
+
+
+
+
+
